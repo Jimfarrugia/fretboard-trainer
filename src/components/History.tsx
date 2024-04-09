76e8e7fac55641028ac9739ad46a6acb8970567a
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-09 14:49:09 +1000

    style History and Leaderboard

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { HiOutlineLightBulb } from "react-icons/hi";
 
 export default function History() {
   return (
     <>
-      <div className="flex justify-between items-center pt-12 pb-8">
-        <h2 className="font-bold text-lg text-dark-heading">History</h2>
-        <p className="text-xs">
+      <div className="sm:flex sm:justify-between sm:items-center pt-12 pb-8">
+        <h2 className="font-bold text-lg mb-1 sm:mb-0 text-light-heading dark:text-dark-heading">
+          History
+        </h2>
+        <p className="flex gap-1 items-center text-xs">
+          <HiOutlineLightBulb className="text-lg text-light-highlight dark:text-dark-highlight" />
           <Link
-            className="font-bold underline hover:text-dark-hover"
+            className="font-bold underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
             href="signin"
           >
             Sign in
@@ -16,8 +20,8 @@ export default function History() {
         </p>
       </div>
       <table className="table-auto w-full text-xs">
-        <thead className="text-left text-dark-heading">
-          <tr className="border-b-2 border-dark-darkerBg">
+        <thead className="text-left text-light-heading dark:text-dark-heading">
+          <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
             <th className="p-2">Date</th>
             <th className="p-2">Instrument</th>
             <th className="p-2">Tuning</th>
@@ -25,7 +29,7 @@ export default function History() {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b-2 border-dark-darkerBg">
+          <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
             <td className="px-2 py-4">2024-07-29</td>
             <td className="px-2 py-4">Guitar</td>
             <td className="px-2 py-4">E Standard</td>
--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { HiOutlineLightBulb } from "react-icons/hi";
 
 export default function Leaderboard() {
   return (
     <>
-      <div className="flex justify-between items-center pt-12 pb-8">
-        <h2 className="font-bold text-lg text-dark-heading">Leaderboard</h2>
-        <p className="text-xs">
+      <div className="sm:flex sm:justify-between sm:items-center pt-12 pb-8">
+        <h2 className="font-bold text-lg mb-1 sm:mb-0 text-light-heading dark:text-dark-heading">
+          Leaderboard
+        </h2>
+        <p className="flex gap-1 items-center text-xs">
+          <HiOutlineLightBulb className="text-lg text-light-highlight dark:text-dark-highlight" />
           <Link
-            className="font-bold underline hover:text-dark-hover"
+            className="font-bold underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
             href="signin"
           >
             Sign in
@@ -16,9 +20,9 @@ export default function Leaderboard() {
         </p>
       </div>
       <table className="table-auto w-full text-xs">
-        <thead className="text-left text-dark-heading">
-          <tr className="border-b-2 border-dark-darkerBg">
-            <th className="p-2">Date</th>
+        <thead className="text-left text-light-heading dark:text-dark-heading">
+          <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
+            <th className="p-2 hidden sm:table-cell">Date</th>
             <th className="p-2">Username</th>
             <th className="p-2">Instrument</th>
             <th className="p-2">Tuning</th>
@@ -26,8 +30,8 @@ export default function Leaderboard() {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b-2 border-dark-darkerBg">
-            <td className="px-2 py-4">2024-07-29</td>
+          <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
+            <td className="px-2 py-4 hidden sm:table-cell">2024-07-29</td>
             <td className="px-2 py-4">SRV_FAN</td>
             <td className="px-2 py-4">Guitar</td>
             <td className="px-2 py-4">E Standard</td>
