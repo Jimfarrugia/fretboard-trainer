39f4986366ef1f9a832241760497083464c5d5a5
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-09 14:48:32 +1000

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,32 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <nav className="flex justify-between items-center my-12 text-sm">
+    <nav className="flex justify-between items-center mt-16 px-4 py-6 text-sm bg-light-darkerBg dark:bg-dark-darkerBg">
       <div>
         <Link
           href="https://jimfarrugia.com.au"
-          className="underline hover:text-dark-hover"
+          className="underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
         >
           Jim Farrugia
         </Link>
       </div>
       <div className="flex gap-4">
-        <Link href="/about" className="underline hover:text-dark-hover">
+        <Link
+          href="/about"
+          className="underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
+        >
           About
         </Link>
-        <Link href="/tips" className="underline hover:text-dark-hover">
+        <Link
+          href="/tips"
+          className="underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
+        >
           Tips
         </Link>
-        <Link href="/signin" className="underline hover:text-dark-hover">
+        <Link
+          href="/signin"
+          className="underline text-light-link hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
+        >
           Sign In
         </Link>
       </div>
