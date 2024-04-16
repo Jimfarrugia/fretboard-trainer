66ad372a958f3e17093d252e8fd68836064ad84a
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-16 22:48:33 +1000

    prevent prisma client creating multiple instances

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { db } from "@/lib/db";
 
-const prisma = new PrismaClient();
+const prisma = db;
 
 export const {
   handlers: { GET, POST },
