0b249c1ab37f38e0b9591cef5acc111121008ff5
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-16 23:18:50 +1000

    add optional properties to Score interface

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,7 +1,12 @@
 export interface Score {
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
   points: number;
   instrument: string;
   tuning: string;
   timestamp: string;
+  published?: boolean;
+  username?: string | null;
   userId?: string;
 }
