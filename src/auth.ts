f1763934961d28d473fba126ae076afee52af3df
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-16 23:23:48 +1000

    remove unnecessary var

--- src/auth.ts
+++ src/auth.ts
@@ -3,15 +3,13 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 
-const prisma = db;
-
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
