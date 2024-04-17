657c9059269b3a32cf3bbf029f6bf74b3ea63d36
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-17 19:30:17 +1000

    display top scores in Leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,24 +1,22 @@
-import Link from "next/link";
-import { HiOutlineLightBulb } from "react-icons/hi";
+import { FaMedal } from "react-icons/fa6";
+import { dateFromTimestamp } from "@/helpers";
+import { getTopScores } from "@/actions/getTopScores";
+import LeaderboardHeader from "./LeaderboardHeader";
 
-export default function Leaderboard() {
-  return (
+export default async function Leaderboard() {
+  const scores = (await getTopScores()) || [];
+  const sortedPoints = scores.map((obj) => obj.points).sort((a, b) => b - a);
+  const pointsSet = new Set(sortedPoints);
+  const uniquePoints = Array.from(pointsSet);
+  const goldScore = uniquePoints[0];
+  const silverScore = uniquePoints[1];
+  const bronzeScore = uniquePoints[2];
+
+  return !scores.length ? (
+    <></>
+  ) : (
     <>
-      <div className="pb-8 pt-12 sm:flex sm:items-center sm:justify-between">
-        <h2 className="mb-1 text-lg font-bold text-light-heading dark:text-dark-heading sm:mb-0">
-          Leaderboard
-        </h2>
-        <p className="flex items-center gap-1 text-xs">
-          <HiOutlineLightBulb className="text-lg text-light-highlight dark:text-dark-highlight" />
-          <Link
-            className="font-bold text-light-link underline hover:text-light-hover dark:text-dark-link dark:hover:text-dark-hover"
-            href="/auth/signin"
-          >
-            Sign in
-          </Link>{" "}
-          to publish your scores.
-        </p>
-      </div>
+      <LeaderboardHeader />
       <table className="w-full table-auto text-xs">
         <thead className="text-left text-light-heading dark:text-dark-heading">
           <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
@@ -30,13 +28,33 @@ export default function Leaderboard() {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg">
-            <td className="hidden px-2 py-4 sm:table-cell">2024-07-29</td>
-            <td className="px-2 py-4">SRV_FAN</td>
-            <td className="px-2 py-4">Guitar</td>
-            <td className="px-2 py-4">E Standard</td>
-            <td className="px-2 py-4">45</td>
-          </tr>
+          {scores.map((score) => (
+            <tr
+              key={`topscore-${score.timestamp}`}
+              className="border-b-2 border-light-darkerBg dark:border-dark-darkerBg"
+            >
+              <td className="hidden px-2 py-4 sm:table-cell">
+                {dateFromTimestamp(score.timestamp)}
+              </td>
+              <td className="px-2 py-4">{score.username || "Anonymous"}</td>
+              <td className="px-2 py-4">{score.instrument}</td>
+              <td className="px-2 py-4">{score.tuning}</td>
+              <td className="px-2 py-4">
+                <div className="flex items-center gap-1">
+                  {score.points}
+                  {score.points === goldScore && (
+                    <FaMedal className="text-md text-gold" />
+                  )}
+                  {score.points === silverScore && (
+                    <FaMedal className="text-md text-silver" />
+                  )}
+                  {score.points === bronzeScore && (
+                    <FaMedal className="text-md text-bronze" />
+                  )}
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
