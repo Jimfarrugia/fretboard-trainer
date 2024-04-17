4276129846a4cff599588528654e2353c2147554
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-18 05:17:40 +1000

    add getScoreRanking server action

---
+++ src/actions/getScoreRanking.ts
@@ -0,0 +1,29 @@
+"use server";
+import { db } from "@/lib/db";
+import { Score } from "@/interfaces";
+
+export async function getScoreRanking(points: number) {
+  try {
+    // Get all published scores in order of highest points
+    const scores = await db.score.findMany({
+      where: { published: true },
+      orderBy: { points: "desc" },
+    });
+
+    if (!scores) {
+      throw new Error("No scores found.");
+    }
+
+    // Create a set of points values
+    const sortedPoints = scores.map((obj) => obj.points).sort((a, b) => b - a);
+    const pointsSet = new Set(sortedPoints);
+    const uniquePoints = Array.from(pointsSet);
+
+    // Find the number of points values higher than the given points value
+    const higherPoints = uniquePoints.filter((p: number) => p > points).length;
+
+    return higherPoints + 1;
+  } catch (e) {
+    console.error("Failed to get score ranking.", e);
+  }
+}
