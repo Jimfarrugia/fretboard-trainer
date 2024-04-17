9ed23643130e89ef3680a0e78193010b44095f05
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-18 05:16:49 +1000

    add ordinal helper

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -106,3 +106,10 @@ export const sortScoresByTimestamp = (scores: Score[]) => {
 export const findHighScore = (scores: Score[]) => {
   return scores.length ? Math.max(...scores.map((s: Score) => s.points)) : 0;
 };
+
+// Append a number with its ordinal suffix (1st, 2nd, 3rd, etc.)
+export const ordinal = (n: number) => {
+  const s = ["th", "st", "nd", "rd"];
+  const v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
