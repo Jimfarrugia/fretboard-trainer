523cfab31eda7dd9157f122428f1bc30efd43dd2
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-20 01:58:30 +1000

    add default settings to constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -20,3 +20,11 @@ export const tunings = [
   { name: "Drop D", strings: ["E", "B", "G", "D", "A", "D"] },
   { name: "D Standard", strings: ["D", "A", "F", "C", "G", "D"] },
 ];
+
+export const defaultSettings = {
+  instrument: instruments[0],
+  tuning: tunings[0],
+  enabledStrings: [true, true, true, true, true, true],
+  sharps: true,
+  flats: false,
+};
