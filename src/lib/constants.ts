d85f05ef27332fdf4b5bda9731a3b71bb981568f
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-04-19 20:02:39 +1000

    add constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const notes = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+];
+
+export const instruments = ["guitar", "bass"] as const;
+
+export const tunings = [
+  { name: "E Standard", notes: ["E", "A", "D", "G", "B", "E"] },
+  { name: "Drop D", notes: ["D", "A", "D", "G", "B", "E"] },
+  { name: "D Standard", notes: ["D", "G", "C", "F", "A", "D"] },
+];
