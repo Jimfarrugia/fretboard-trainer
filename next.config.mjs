0cdcc72a3ab624f5ef510920100fa13e71e04d25
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-05-01 20:10:39 +1000

    configure strict content security policy

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,37 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    // Strict CSP
+    // + https://va.vercel-scripts.com added for Vercel Speed Insights
+    // + data: lh3.googleusercontent.com; added for Google profile images
+    // + https://www.youtube.com; added for YouTube player embeds
+    const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com;
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' blob: data: lh3.googleusercontent.com;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-src 'self' https://www.youtube.com;
+        frame-ancestors 'none' https://www.youtube.com;
+        upgrade-insecure-requests;
+    `;
+
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
