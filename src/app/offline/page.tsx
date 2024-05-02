77d0588b040776f6367ee75f1752896ef395da13
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-05-02 16:20:46 +1000

    add offline homepage

---
+++ src/app/offline/page.tsx
@@ -0,0 +1,11 @@
+import History from "@/components/History";
+import Game from "@/components/Game";
+
+export default async function Offline() {
+  return (
+    <main>
+      <Game />
+      <History />
+    </main>
+  );
+}
