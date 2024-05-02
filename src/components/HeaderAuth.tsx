2e7daca7d26e08ed064b01c4edd4a6d5107a74f0
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-05-02 21:31:55 +1000

    display offline indicator instead of auth btns when offline

--- src/components/HeaderAuth.tsx
+++ src/components/HeaderAuth.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { MdSignalWifiConnectedNoInternet0 } from "react-icons/md";
+import { useOnlineStatus } from "@/lib/hooks";
 import UserModal from "./UserModal";
 import SignOut from "./SignOut";
 
@@ -10,6 +12,26 @@ export default function HeaderAuth({
   includeUserModal?: boolean;
 }) {
   const session = useSession();
+  const { isOnline } = useOnlineStatus();
+
+  if (isOnline === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  // If offline, show an indicator (and tooltip on non-mobile) instead of auth buttons
+  if (!isOnline) {
+    return (
+      <div
+        className="tooltip tooltip-left hidden cursor-default [--tooltip-color:theme(colors.light.darkerBg)] [--tooltip-text-color:theme(colors.light.body)] dark:[--tooltip-color:theme(colors.dark.darkerBg)] dark:[--tooltip-text-color:theme(colors.dark.body)] sm:inline-block"
+        data-tip="You're offline at the moment. Your scores will be saved as soon as you're back online and signed-in."
+      >
+        <p className="flex items-center gap-2 text-error">
+          <MdSignalWifiConnectedNoInternet0 className="text-lg" />
+          <span className="animate-warning-text">Offline</span>
+        </p>
+      </div>
+    );
+  }
 
   if (includeUserModal) {
     return session?.data?.user ? (
