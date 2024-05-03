c33aa96782444df15cc88026585c6fd351423bda
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-05-03 14:55:02 +1000

    add noscript element/message to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BiError } from "react-icons/bi";
 import History from "@/components/History";
 import Leaderboard from "@/components/Leaderboard";
 import Game from "@/components/Game";
@@ -7,6 +8,21 @@ export default async function Home() {
   const topScores = await getTopScores();
   return (
     <main>
+      <noscript>
+        <div
+          role="alert"
+          className="alert alert-error mb-6 bg-error text-dark-darkerBg"
+        >
+          <BiError className="text-4xl" />
+          <span>
+            <span className="font-bold">
+              Scripts are disabled in your browser.{" "}
+            </span>
+            <br />
+            This app will not work unless JavaScript is enabled.
+          </span>
+        </div>
+      </noscript>
       <Game />
       <History />
       <Leaderboard topScores={topScores} />
