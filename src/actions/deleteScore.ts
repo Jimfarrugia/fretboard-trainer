40f325817f8b9e4a45c044f43881beffbc8f96a2
Jim Farrugia <the.jim.farrugia@gmail.com>
2024-05-07 17:39:32 +1000

    add deleteScore server action

---
+++ src/actions/deleteScore.ts
@@ -0,0 +1,19 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { db } from "@/lib/db";
+import { Score } from "@/lib/types";
+
+export async function deleteScore(userId: string, score: Score) {
+  try {
+    const { timestamp } = score;
+
+    await db.score.deleteMany({ where: { userId, timestamp } });
+
+    if (score.published) {
+      // Revalidate to update the leaderboard
+      revalidatePath("/");
+    }
+  } catch (e) {
+    console.error("Failed to delete the score in the database.", e);
+  }
+}
